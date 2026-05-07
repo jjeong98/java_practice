a2d8379b89f0c664ee9ed3ebc97d3c30f6fe410d
jjeong <jjeong98@outlook.com>
2026-05-07 11:19:43 +0900

    자바 반복문 - 컨티뉴

---
+++ src/loop/Continue.java
@@ -0,0 +1,26 @@
+package loop;
+
+public class Continue {
+
+    public static void main(String[] args) {
+
+        for (int i = 1; i <= 10; i++) {
+            // for문에서 continue는 '증감식'이 실행이 된다.
+            // while문에서도 가능하지만, "조건식"식이 실행이 됨
+            if (i == 5) continue;
+
+            System.out.print(i+ " ");
+        }
+
+        System.out.println("\n=========================================================");
+
+        int n = 1;
+        while (n <= 10) {
+            if (n == 5) continue;
+            System.out.print(n+ " ");
+            n++;
+        }
+
+        System.out.println("\n반복문 종료");
+    }
+}
